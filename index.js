81b4be8b219ea58ed881970cc399d08700af0374
Omar Choudhary <ochoudhary@spartaglobal.co>
2017-05-24 23:03:01 +0100

    updated game to make start and quit buttons dissappear using hide function and also added quit page

--- index.js
+++ index.js
@@ -76,66 +76,79 @@ function startGame(){
     $(".start").click(function (event){
     	startGame();	
 	});
-
-    function gunMusic (){
-		var isPlay = false;
-		var sound = document.createElement('audio');
+function hideElements(){
+    
+    $(".start").click(function(){
+        $(".start",).hide();
+    });
+  	
+  	$(".start").click(function(){
+        $("#side",).hide();
+    });
+}
+hideElements();
+function gunMusic (){
+	var isPlay = false;
+	var sound = document.createElement('audio');
 		
-		sound.setAttribute('src', 'gunshot.mp3');
-		$.get();
+	sound.setAttribute('src', 'gunshot.mp3');
+	$.get();
 
-		$('.random').click(function(){
+	$('.random').click(function(){
 		sound.play();
 		isPlay = true;
 	});
 
-		$('.random').click(function(){
+	$('.random').click(function(){
 		sound.play();
 		isPlay = true;
 	});
 	
 	}
-		gunMusic();
+	gunMusic();
 
     // if we have, increment the score or else decrement if friendly
-    function addCounter(){
-		$('.random').click(function () {
+function addCounter(){
+	$('.random').click(function () {
 		var person = $(this)
-		console.log(person.attr("src"))
-		console.log(person.hasClass("bad"))
+		// console.log(person.attr("src"))
+		// console.log(person.hasClass("bad"))
 
 		// check to see if we've shot a bad person
 		if(person.hasClass ('bad')){	
 			counters++;
 			$('.score-count').html(counters);
 		}
+		// else if('.score-count' === 10){
+		// 	console.log("you win");
+		// }
 		else{
 			counters--;
 			$('.score-count').html(counters);
 		}
-
 		var id = '#' + $(this).attr('id');
 		changeImage(id);
 	})
 }
-		addCounter();
+	addCounter();
 
 function makeNewPosition(){
     	// Get viewport dimensions (remove the dimension of the div)
-    	var h = $('#board').height() + 10;
-    	var w = $('#board').width() + 10;
+    var h = $('#board').height() + 10;
+    var w = $('#board').width() + 10;
 
-    	var nh = Math.floor(Math.random() * h);
-   	 	var nw = Math.floor(Math.random() * w);
+    var nh = Math.floor(Math.random() * h);
+   	var nw = Math.floor(Math.random() * w);
     
-    	return [nh,nw];    
-	}
+    return [nh,nw];    
+}
 function animateDiv(){
+    
     $('.random').each(function() {
        var newq = makeNewPosition();
 
       $(this).animate({ top: newq[0]+110, left: newq[1] },3000, function(){
-        animateDiv();        
+       	 animateDiv();        
       });
     })
 }
