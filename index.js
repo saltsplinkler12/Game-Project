753d4d75942988e319bffc1a8b3178d5d04e3867
Omar Choudhary <ochoudhary@spartaglobal.co>
2017-05-25 16:18:05 +0100

    indented main.js page

--- index.js
+++ index.js
@@ -61,18 +61,18 @@ $(function(){
 	}	
 	playMusic();
 
-function startGame(){
-	for(var i = 0; i < 9; i++) {
-		var size = people.length
-		var x = Math.floor(size * Math.random());
-		var newPerson = people[x]
-	
-		$('#randoms' + i).attr('src', newPerson.src);
-	
-	if (newPerson.isBad){
-			$('#randoms' + i).addClass("bad")
-		}
-}
+	function startGame(){
+		for(var i = 0; i < 9; i++) {
+			var size = people.length
+			var x = Math.floor(size * Math.random());
+			var newPerson = people[x]
+		
+			$('#randoms' + i).attr('src', newPerson.src);
+		
+		if (newPerson.isBad){
+				$('#randoms' + i).addClass("bad")
+			}
+	}
 
 	}
     $(".start").click(function (event){
@@ -81,107 +81,106 @@ function startGame(){
     	startGame();	
 	});
 
-function hideElements(){
-    
-    $(".start").click(function(){
-        $(".start").hide();
-    });
-  	
-  	$(".start").click(function(){
-        $("#side").hide();
-    });
+	function hideElements(){
+		    
+		    $(".start").click(function(){
+		        $(".start").hide();
+		    });
+		  	
+		  	$(".start").click(function(){
+		        $("#side").hide();
+		    });
 
-    $(".start").click(function(){
-        $(".again").hide();
-    });
-  
-}
-hideElements();
+		    $(".start").click(function(){
+		        $(".again").hide();
+		    });
+		  
+		}
+		hideElements();
 
-function gunMusic (){
-	var isPlay = false;
-	var sound = document.createElement('audio');
-		
-	sound.setAttribute('src', 'gunshot.mp3');
+	function gunMusic (){
+		var isPlay = false;
+		var sound = document.createElement('audio');
+			
+		sound.setAttribute('src', 'gunshot.mp3');
 
-	$('.random').click(function(){
-		sound.play();
-		isPlay = true;
-	});
-	
-	}
-	gunMusic();
+		$('.random').click(function(){
+			sound.play();
+			isPlay = true;
+		});
+
+		}
+		gunMusic();
 
     // if we have, increment the score or else decrement if friendly
-function addCounter(){
-	$('.random').click(function () {
-		var person = $(this)
-		// console.log(person.attr("src"))
-		// console.log(person.hasClass("bad"))
-		// check to see if we've shot a bad person
-		if(person.hasClass ('bad')){	
-			counters++;
-			$('.score-count').html(counters);
-		}
-		else{
-			counters--;
-			$('.score-count').html(counters);
-		}
-		var id = '#' + $(this).attr('id');
-		changeImage(id);
+	function addCounter(){
+		$('.random').click(function () {
+			var person = $(this)
+			// console.log(person.attr("src"))
+			// console.log(person.hasClass("bad"))
+			// check to see if we've shot a bad person
+			if(person.hasClass ('bad')){	
+				counters++;
+				$('.score-count').html(counters);
+			}
+			else{
+				counters--;
+				$('.score-count').html(counters);
+			}
+			var id = '#' + $(this).attr('id');
+			changeImage(id);
 
-			if(counters === 5) {
-			$('.random').hide();
-			$("#win").show();
-			$(".again").show();
-			$("#counter").hide();
+				if(counters === 5) {
+				$('.random').hide();
+				$("#win").show();
+				$(".again").show();
+				$("#counter").hide();
 
+		}
+		})
+		
 	}
-	})
-	
-}
 	addCounter();
 
-function makeNewPosition(){
-    	// Get viewport dimensions (remove the dimension of the div)
-    var h = $('#board').height() + 10;
-    var w = $('#board').width() + 10;
+	function makeNewPosition(){
+	    	// Get viewport dimensions (remove the dimension of the div)
+	    var h = $('#board').height() + 10;
+	    var w = $('#board').width() + 10;
 
-    var nh = Math.floor(Math.random() * h);
-   	var nw = Math.floor(Math.random() * w);
-    
-    return [nh,nw];    
-}
-
-function animateDiv(){
-    
-    $('.random').each(function() {
-       var newq = makeNewPosition();
-
-      $(this).animate({ top: newq[0]+110, left: newq[1] },3000, function(){
-       	 animateDiv();        
-      });
-    })
-}
-
-function decTime(){
-	counter = counter -1;
-	$('#counter').html(counter);
-	
-	if(counter <= 0){
-		clearInterval(timer);
-		$('#counter').html("OUT OF TIME!!");
-		
-		$(".again").show();
-
-		if(counter === 0) {
-		$('.random').hide();
+	    var nh = Math.floor(Math.random() * h);
+	   	var nw = Math.floor(Math.random() * w);
+	    
+	    return [nh,nw];    
 	}
-		
-		return;
 
+	function animateDiv(){
+	    $('.random').each(function() {
+	       var newq = makeNewPosition();
+
+	      $(this).animate({ top: newq[0]+110, left: newq[1] },3000, function(){
+	       	 animateDiv();        
+	      });
+	    })
 	}
-}
+
+	function decTime(){
+		counter = counter -1;
+		$('#counter').html(counter);
+		
+		if(counter <= 0){
+			clearInterval(timer);
+			$('#counter').html("OUT OF TIME!!");
+			
+			$(".again").show();
+
+			if(counter === 0) {
+			$('.random').hide();
+		}
+			
+			return;
+
+		}
+	}
 
  
 
